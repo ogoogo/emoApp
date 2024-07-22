98c7ef38cd8694a832f0c37d37d0aef38d9bd5ee
annazip <annamori.nima@gmail.com>
2024-07-22 12:18:57 +0900

    微調整

--- reflectColor/Reflect/RecordStartViewController.swift
+++ reflectColor/Reflect/RecordStartViewController.swift
@@ -21,6 +21,7 @@ class RecordStartViewController: UIViewController, AVAudioRecorderDelegate{
     @IBOutlet var nextButton: UIBarButtonItem!
     @IBOutlet var eachBackgrounds: [UILabel]!
     @IBOutlet var answerBackgound: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
     
     
     override func viewDidLoad() {
@@ -30,23 +31,29 @@ class RecordStartViewController: UIViewController, AVAudioRecorderDelegate{
         setupBackgrounds()
         
         
-                AVAudioSession.sharedInstance().requestRecordPermission { granted in
-                    if granted {
-                        print("Permission granted")
-                    } else {
-                        print("Permission denied")
-                    }
-    }
-    
-            let audioSession = AVAudioSession.sharedInstance()
-            do {
-                try audioSession.setCategory(.playAndRecord, mode: .default)
-                try audioSession.setActive(true)
-            } catch {
-                print("Failed to set audio session category and mode: \(error)")
-                return
+        AVAudioSession.sharedInstance().requestRecordPermission { granted in
+            if granted {
+                print("Permission granted")
+            } else {
+                print("Permission denied")
             }
         }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(.playAndRecord, mode: .default)
+            try audioSession.setActive(true)
+        } catch {
+            print("Failed to set audio session category and mode: \(error)")
+            return
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        recordButton.frame = CGRect(x:172,y:687,width:50,height:50)
+        recordButton.layer.masksToBounds = true
+        recordButton.layer.cornerRadius = 25.0
+    }
     
     
     func setupBackgrounds() {
@@ -54,78 +61,79 @@ class RecordStartViewController: UIViewController, AVAudioRecorderDelegate{
             label.layer.cornerRadius = 10
             label.clipsToBounds = true
         }
-        answerBackgound.layer.borderColor = UIColor(red: 25/255, green: 44/255, blue: 112/255, alpha: 1.0).cgColor
-        answerBackgound.layer.borderWidth = 2.0
-        answerBackgound.clipsToBounds = true
+//        answerBackgound.layer.borderColor = UIColor(red: 25/255, green: 44/255, blue: 112/255, alpha: 1.0).cgColor
+//        answerBackgound.layer.borderWidth = 2.0
+//        answerBackgound.clipsToBounds = true
     }
     
-            @IBAction func startRecording(_ sender: UIButton) {
-                if !isRecording {
-        //            スタートする時
-                    isRecording = true
-                    let audioFilename = getDocumentsDirectory().appendingPathComponent("recording2.raw")
-                    recordingURL = audioFilename
-        
-                    let settings = [
-                        AVFormatIDKey: kAudioFormatLinearPCM,
-                        AVSampleRateKey: 16000,
-                        AVNumberOfChannelsKey: 1,
-                        AVLinearPCMBitDepthKey: 16,
-                        AVLinearPCMIsBigEndianKey: false,
-                        AVLinearPCMIsFloatKey: false
-                    ] as [String : Any]
-        
-                    do {
-                        audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
-                        audioRecorder?.delegate = self
-                        audioRecorder?.record()
-                        print("Recording started")
-                    } catch {
-                        print("Failed to start recording: \(error)")
-                    }
-                } else {
-        //            録音停止する時
-                    audioRecorder?.stop()
-                    audioRecorder = nil
-                    print("Recording stopped")
-        //            このrequest()で下に書いてる関数読んで，API通信をしている！
-                    request()
-                    isRecording = false
-                }
+    @IBAction func startRecording(_ sender: UIButton) {
+        if !isRecording {
+            //            スタートする時
+            isRecording = true
+            let audioFilename = getDocumentsDirectory().appendingPathComponent("recording2.raw")
+            recordingURL = audioFilename
+            
+            let settings = [
+                AVFormatIDKey: kAudioFormatLinearPCM,
+                AVSampleRateKey: 16000,
+                AVNumberOfChannelsKey: 1,
+                AVLinearPCMBitDepthKey: 16,
+                AVLinearPCMIsBigEndianKey: false,
+                AVLinearPCMIsFloatKey: false
+            ] as [String : Any]
+            
+            do {
+                audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+                audioRecorder?.delegate = self
+                audioRecorder?.record()
+                print("Recording started")
+            } catch {
+                print("Failed to start recording: \(error)")
             }
-        
-            func request() {
-                    APIManager.shared.request(audioURL: recordingURL) { happiness, disgust, neutral, sadness, anger, text in
-                        if let happiness = happiness, let disgust = disgust, let neutral = neutral, let sadness = sadness, let anger = anger, let text = text {
-        
-        //                    この辺で感情の値を取り出せる！好きに使ってね！
-                            print("Happiness: \(happiness)")
-                            print("Disgust: \(disgust)")
-                            print("Neutral: \(neutral)")
-                            print("Sadness: \(sadness)")
-                            print("Anger: \(anger)")
-        //                    textも出せる！
-                            print("Text: \(text)")
-                            DispatchQueue.main.async {
-        //                        例えば取り出せた文章を出せるよね
-                                self.answerBackgound.text = text
-        //                        例えばここで次へボタン復活させたら次画面いける
-                                self.nextButton.isHidden = false
-        
-                            }
-                        } else {
-                            print("Failed to retrieve emotion or text")
-                        }
-                    }
-                }
-        
-            func getDocumentsDirectory() -> URL {
-                let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-                return paths[0]
-            }
-        
-        
-        
-        
+            
+        } else {
+            //            録音停止する時
+            audioRecorder?.stop()
+            audioRecorder = nil
+            print("Recording stopped")
+            //            このrequest()で下に書いてる関数読んで，API通信をしている！
+            request()
+            isRecording = false
+        }
     }
+    
+    func request() {
+        APIManager.shared.request(audioURL: recordingURL) { happiness, disgust, neutral, sadness, anger, text in
+            if let happiness = happiness, let disgust = disgust, let neutral = neutral, let sadness = sadness, let anger = anger, let text = text {
+                
+                //                    この辺で感情の値を取り出せる！好きに使ってね！
+                print("Happiness: \(happiness)")
+                print("Disgust: \(disgust)")
+                print("Neutral: \(neutral)")
+                print("Sadness: \(sadness)")
+                print("Anger: \(anger)")
+                //                    textも出せる！
+                print("Text: \(text)")
+                DispatchQueue.main.async {
+                    //                        例えば取り出せた文章を出せるよね
+                    self.answerBackgound.text = text
+                    //                        例えばここで次へボタン復活させたら次画面いける
+                    self.nextButton.isHidden = false
+                    
+                }
+            } else {
+                print("Failed to retrieve emotion or text")
+            }
+        }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    
+    
+    
+}
 
